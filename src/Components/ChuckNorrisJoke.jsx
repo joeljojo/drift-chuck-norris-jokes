fd3bac15d47cd54bef27bafedc63123596e50ae2
joeljojo <joelpamphyl@gmail.com>
2023-10-04 09:32:15 +0300

    Add Chuck Norris Get Joke by Category

--- src/Components/ChuckNorrisJoke.jsx
+++ src/Components/ChuckNorrisJoke.jsx
@@ -1,21 +1,36 @@
 import { useState, useEffect } from "react";
 import JokeDisplay from "./JokeDisplay";
+import JokeCategory from "./JokeCategory";
 import chuckNorrisApi from "../Services/ChuckNorrisApi";
 
 const ChuckNorrisJoke = () => {
   const [joke, setJoke] = useState("");
+  const [category, setCategory] = useState("");
 
   const getRandomJoke = async () => {
     const randomJoke = await chuckNorrisApi.fetchRandomJoke();
     setJoke(randomJoke.value);
   };
 
+  const getJokeByCategory = async () => {
+    if (category) {
+      const jokeInCategory = await chuckNorrisApi.fetchJokeByCategory(category);
+      setJoke(jokeInCategory.value);
+    }
+  };
+
   useEffect(() => {
     getRandomJoke();
   }, []);
+
   return (
     <div>
       <h1>Chuck Norris Jokes</h1>
+      <JokeCategory
+        category={category}
+        setCategory={setCategory}
+        getJokeByCategory={getJokeByCategory}
+      />
       <JokeDisplay joke={joke} />
       <button onClick={getRandomJoke}>Get Ramdom Joke</button>
     </div>
---
+++ src/Components/JokeCategory.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import chuckNorrisApi from "../Services/ChuckNorrisApi";
+
+const JokeCategory = ({ category, setCategory, getJokeByCategory }) => {
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = async () => {
+    const jokeCategories = await chuckNorrisApi.fetchCategories();
+    setCategories(jokeCategories);
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+  return (
+    <div>
+      <label htmlFor="categories">Select category</label>
+      <select
+        name="categories"
+        id="categories"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value="">Select category</option>
+        {categories.map((cat) => (
+          <option key={cat} value={cat}>
+            {cat}
+          </option>
+        ))}
+      </select>
+      <button onClick={getJokeByCategory}>Get Joke</button>
+    </div>
+  );
+};
+export default JokeCategory;
